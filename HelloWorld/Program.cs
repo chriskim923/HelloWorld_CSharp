4e9978f144ebd05a677fbf7bafd1b52d7a5917b3
Chris Kim <chris.kim923@gmail.com>
2017-07-11 09:52:18 -0400

    initial project version

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿// A Hello World! program in C#
+using System; // Allows me to directly use the System classes and methods without fully qualifying them.
+namespace HelloWorld
+{
+    class Hello
+    {
+        static void Main()
+        {
+            System.Console.WriteLine("Hello World!");
+
+            // Keep the console window open in debug mode. 
+            // This time, omitting System. because I already declared "using System"
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
+
+// "That's one small step for man. One giant leap for mankind." - Neil Armstrong
